066c21f6c86c46c25f7fed580e625383c88284a5
Emilio <emilio.tenaromero@student.hogent.be>
2023-10-18 12:22:25 +0200

    Clean-up boilerplate

---
+++ src/Domain/Dice.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain;
+
+internal class Dice
+{
+    private readonly Random _randomizer = new();
+
+    internal int Dots { get; private set; } = 6;
+
+    internal Dice()
+    {
+       // Dots = 6;  kan ook
+    }
+
+    internal void Roll()
+    {
+        Dots = _randomizer.Next(1, 7);
+    }
+}
